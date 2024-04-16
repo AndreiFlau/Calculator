19d91b21b1ffe7ea7095bfdd86597cd6db38cc8c
Andrei <andreiflausino@live.com>
2024-04-16 20:54:26 -0300

    Initial state

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ function divide(num1, num2) {
 }
 
 function operate(operator, num1, num2) {
+  num1 = Number(num1);
+  num2 = Number(num2);
   if (operator === "+") {
     return add(num1, num2);
   }
@@ -29,47 +31,62 @@ function operate(operator, num1, num2) {
   }
 }
 
+function calculate() {
+  if (displayValue.includes("+")) {
+    let array = displayValue.split("+");
+    if (array.length >= 1) {
+      display.textContent = operate("+", array[0], array[1]);
+      displayValue = display.textContent;
+    }
+  }
+  else if (displayValue.includes("-")) {
+    let array = displayValue.split("-");
+    if (array.length >= 1) {
+      display.textContent = operate("-", array[0], array[1]);
+      displayValue = display.textContent;
+    }
+  }
+  else if (displayValue.includes("*")) {
+    let array = displayValue.split("*");
+    if (array.length >= 1) {
+      display.textContent = operate("*", array[0], array[1]);
+      displayValue = display.textContent;
+    }
+  }
+  else if (displayValue.includes("/")) {
+    let array = displayValue.split("/");
+    if (array.length >= 1) {
+      display.textContent = operate("/", array[0], array[1]);
+      displayValue = display.textContent;
+    }
+  }
+}
+
 function appendToDisplay(symbol) {
   const operators = ["+", "-", "*", "/"];
   const numValue = symbol.textContent;
   display.textContent = "";
-  let displayArray = displayValue.split("+");
   symbol.addEventListener("click", () => {
-    if (operators.some(operator => displayValue.includes(operator))) {
-      if (displayArray.length > 1) {
-        if (displayValue.includes("+")) {
-          let array = displayValue.split("+");
-          if (array.length > 1) {
-            display.textContent = operate("+", array[0], array[1]);
-            displayValue = display.textContent;
-          }
-        }
-        else if (displayValue.includes("-")) {
-          let array = displayValue.split("-");
-          if (array.length > 1) {
-            display.textContent = operate("-", array[0], array[1]);
-            displayValue = display.textContent;
-          }
-        }
-        else if (displayValue.includes("*")) {
-          let array = displayValue.split("*");
-          if (array.length > 1) {
-            display.textContent = operate("*", array[0], array[1]);
-            displayValue = display.textContent;
-          }
-        }
-        else if (displayValue.includes("/")) {
-          let array = displayValue.split("/");
-          if (array.length > 1) {
-            display.textContent = operate("/", array[0], array[1]);
-            displayValue = display.textContent;
-          }
-        }
-      }
+    displayValue = display.textContent;
+    let foundOperator = operators.find(
+      operator => displayValue.includes(operator));
+    if (operators.includes(foundOperator)) {
+      foundOperator = "\\" + foundOperator;
+    }
+    let array = displayValue.split(new RegExp(`(${foundOperator})`));
+    console.log(array);
+    if (symbol.textContent === "X") {
+      displayValue = "";
+      display.textContent = "";
+    }
+    else if (symbol.textContent === "=") {
+      calculate();
+    }
+    else if (array.length > 3) {
+      calculate();
     }
     else {
       display.textContent += numValue;
-      displayValue = display.textContent;
     }
   });
 }
@@ -94,11 +111,15 @@ const number6 = document.querySelector("#six");
 const number7 = document.querySelector("#seven");
 const number8 = document.querySelector("#eight");
 const number9 = document.querySelector("#nine");
+const zero = document.querySelector("#zero");
 
 const addElement = document.querySelector("#sum");
 const subtractElement = document.querySelector("#subtract");
 const multiplyElement = document.querySelector("#multiply");
 const divideElement = document.querySelector("#divide");
+
+const calculateElement = document.querySelector("#calculate");
+const clear = document.querySelector("#clear");
 
 appendToDisplay(number1);
 appendToDisplay(number2);
@@ -109,9 +130,12 @@ appendToDisplay(number6);
 appendToDisplay(number7);
 appendToDisplay(number8);
 appendToDisplay(number9);
+appendToDisplay(zero);
 appendToDisplay(addElement);
 appendToDisplay(subtractElement);
 appendToDisplay(multiplyElement);
 appendToDisplay(divideElement);
+appendToDisplay(calculateElement);
+appendToDisplay(clear);
 
 console.log(operate(operator, num1, num2));
